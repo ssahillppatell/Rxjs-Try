babc49913bb41f447417b027fca6ca536fa5d451
Pagal <ssahillppatell@gmail.com>
2022-02-02 01:34:00 +0530

    Basic functionality implemented

--- src/components/ObservableBox.vue
+++ src/components/ObservableBox.vue
@@ -1,8 +1,46 @@
 <template>
-    <div class="click-me">
+    <div ref="clickMe" class="click-me">
         Click Me
+        <br>
+        {{numberOfClicks}}
     </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { asyncScheduler, auditTime, buffer, bufferCount, bufferTime, bufferToggle, bufferWhen, connect, debounce, debounceTime, delay, EMPTY, filter, fromEvent, interval, map, pluck, scan, takeUntil, throttle, throttleTime, timeInterval, timer } from 'rxjs'
+import { useToast } from 'vue-toastification';
+
+const clickMe = ref<HTMLDivElement | null>(null)
+const clickType = ref('')
+const numberOfClicks = ref(0)
+const toast = useToast()
+
+onMounted(() => {
+    const myObservable = fromEvent(clickMe.value, 'click')
+
+    let myPipe = myObservable.pipe(
+        connect(val => val.pipe(
+            buffer(val.pipe(
+                throttleTime(2000, asyncScheduler, { leading: false, trailing: true })
+            ))
+        )),
+        map((val) => {
+            return val.length
+        })
+    )
+
+    myPipe.subscribe((value) => {
+        if(value >= 2) {
+            toast.success("Double Click!")
+        } else {
+            toast.info("Single Click!")
+        }
+    })
+})
+
+
+</script>
 
 <style lang="postcss">
     .click-me {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './index.css'
 
-createApp(App).mount('#app')
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+
+createApp(App).use(Toast).mount('#app')
