ca69f15dd2941aad30c58db1754d65c046f1317a
Pagal <ssahillppatell@gmail.com>
2022-02-01 22:46:55 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ObservableBox />
+</template>
+
+<script setup lang="ts">
+import ObservableBox from '@/components/ObservableBox.vue'
+</script>
+
+<style lang="postcss">
+#app {
+	@apply h-screen w-screen flex items-center justify-center;
+}
+</style>
---
+++ src/components/ObservableBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="click-me">
+        Click Me
+    </div>
+</template>
+
+<style lang="postcss">
+    .click-me {
+        @apply h-[300px] w-[600px] flex justify-center items-center;
+        @apply border-2 border-gray-600 bg-yellow-100 cursor-pointer rounded;
+    }
+</style>
